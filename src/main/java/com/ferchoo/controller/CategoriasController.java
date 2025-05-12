bb068b299facd5ce03f685ec06bad3bfc4adbdb3
fernando corral <146652466+fersho101@users.noreply.github.com>
2025-05-11 21:00:19 -0700

    primer commit

---
+++ src/main/java/com/ferchoo/controller/CategoriasController.java
@@ -0,0 +1,31 @@
+package com.ferchoo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/categorias")
+public class CategoriasController {
+
+	@GetMapping("/index")
+	public String mostrarIndex(Model model) {
+		return "categorias/listCategorias";
+	}
+
+	@GetMapping("/create")
+	public String crear() {
+		return "categorias/formCategoria";
+	}
+
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
+		System.out.println("Categoria: " + nombre);
+		System.out.println("Descripcion: " + descripcion);
+		return "categorias/listCategorias";
+	}
+
+}
---
+++ src/test/java/com/ferchoo/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferchoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
